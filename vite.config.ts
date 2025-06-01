14f5e861281a5e2ed148a5c62615bcf321e357e5
Visnu Pitiyanuvath <visnup@gmail.com>
2025-06-01 01:52:26 -0700

    build for new.html

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,24 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { dirname, resolve } from "node:path";
+import { fileURLToPath } from "node:url";
 
 // https://vite.dev/config/
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 export default defineConfig({
   plugins: [react()],
   define: {
     "process.env.TLDRAW_WORKER_URL": JSON.stringify(
       process.env.TLDRAW_WORKER_URL ?? "http://localhost:5172",
     ),
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, "index.html"),
+        new: resolve(__dirname, "new.html"),
+      },
+    },
   },
 });
