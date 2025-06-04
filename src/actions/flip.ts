3a8aa1662fd5243471c49325f1647c4282440bc2
Visnu Pitiyanuvath <visnup@gmail.com>
2025-06-03 23:21:46 -0700

    flip reverses z index too

--- src/actions/flip.ts
+++ src/actions/flip.ts
@@ -6,18 +6,30 @@ export const flip: (editor: Editor) => TLUiActionsContextType = (editor) => ({
     label: "Flip",
     kbd: "f",
     onSelect: () => {
-      editor.updateShapes(
-        [...editor.getSelectedShapes(), editor.getHoveredShape()]
-          .filter(
-            (s): s is TLUnknownShape & { props: { faceUp: boolean } } =>
-              !!s && "faceUp" in s.props,
-          )
-          .map((s) => ({
+      const shapes = [
+        ...editor.getSelectedShapes(),
+        editor.getHoveredShape(),
+      ].filter(
+        (s): s is TLUnknownShape & { props: { faceUp: boolean } } =>
+          !!s && "faceUp" in s.props,
+      );
+      editor.run(() => {
+        editor.updateShapes(
+          shapes.map((s) => ({
             id: s.id,
             type: s.type,
             props: { faceUp: !s.props.faceUp },
           })),
-      );
+        );
+        if (shapes.length <= 1) return;
+        const ids = new Set(shapes.map((s) => s.id));
+        const order = editor
+          .getCurrentPageShapesSorted()
+          .map((s) => s.id)
+          .filter((id) => ids.has(id))
+          .reverse();
+        for (const id of order) editor.bringToFront([id]);
+      });
     },
   },
 });
