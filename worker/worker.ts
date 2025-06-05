2108d9eba92b45cadb4763bfdff4b805eca97b8b
Visnu Pitiyanuvath <visnup@gmail.com>
2025-06-05 02:31:53 -0700

    remove unused worker code

--- worker/worker.ts
+++ worker/worker.ts
@@ -1,6 +1,4 @@
-import { handleUnfurlRequest } from "cloudflare-workers-unfurl";
 import { AutoRouter, cors, error, IRequest } from "itty-router";
-import { handleAssetDownload, handleAssetUpload } from "./assetUploads";
 import { Environment } from "./types";
 
 // make sure our sync durable object is made available to cloudflare
@@ -25,16 +23,7 @@ const router = AutoRouter<IRequest, [env: Environment, ctx: ExecutionContext]>({
       headers: request.headers,
       body: request.body,
     });
-  })
-
-  // assets can be uploaded to the bucket under /uploads:
-  .post("/uploads/:uploadId", handleAssetUpload)
-
-  // they can be retrieved from the bucket too:
-  .get("/uploads/:uploadId", handleAssetDownload)
-
-  // bookmarks need to extract metadata from pasted URLs:
-  .get("/unfurl", handleUnfurlRequest);
+  });
 
 // export our router for cloudflare
 export default router;
