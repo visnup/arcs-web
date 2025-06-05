32809267ee22c054f714a273743bd169acc275c5
Visnu Pitiyanuvath <visnup@gmail.com>
2025-06-05 01:22:51 -0700

    don't raise on select

--- src/App.tsx
+++ src/App.tsx
@@ -71,23 +71,6 @@ export default function App() {
     if (window.location.search === "?new") setup(editor);
     editor.user.updateUserPreferences({ colorScheme: "dark" });
     editor.zoomToFit();
-
-    const cleanups: (() => void)[] = [];
-
-    // Bring selected shapes to front
-    cleanups.push(
-      editor.sideEffects.registerAfterChangeHandler(
-        "instance_page_state",
-        (p, n, source) => {
-          if (source === "user" && p.selectedShapeIds !== n.selectedShapeIds)
-            editor.bringToFront(n.selectedShapeIds);
-        },
-      ),
-    );
-
-    return () => {
-      for (const fn of cleanups) fn();
-    };
   }, []);
 
   return (
