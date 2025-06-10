535be7dfcf7f9cf2ed5707cad2a2e130780c5a93
Visnu Pitiyanuvath <visnup@gmail.com>
2025-06-10 15:02:40 -0700

    vite-plugin-vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import vercel from "vite-plugin-vercel";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), vercel()],
   define: {
     "process.env.TLDRAW_WORKER_URL": JSON.stringify(
       process.env.TLDRAW_WORKER_URL ?? "http://localhost:5172",
