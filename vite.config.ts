e32cc69c236b45f912fda61bc45b61a8172081ae
Visnu Pitiyanuvath <visnup@gmail.com>
2025-06-10 16:31:00 -0700

    chunk vendor

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,15 @@ import vercel from "vite-plugin-vercel";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), vercel()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) return "vendor";
+        },
+      },
+    },
+  },
   define: {
     "process.env.TLDRAW_WORKER_URL": JSON.stringify(
       process.env.TLDRAW_WORKER_URL ?? "http://localhost:5172",
