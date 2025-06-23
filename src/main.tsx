e99a3c72467a5088248b7e58aa0beb5edb4ba469
Visnu Pitiyanuvath <visnup@gmail.com>
2025-06-24 07:37:29 +0900

    vercel analytics too

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,14 @@
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import App from "./App.tsx";
-import { SpeedInsights } from "@vercel/speed-insights/react";
+import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
     <SpeedInsights />
+    <Analytics />
   </StrictMode>,
 );
