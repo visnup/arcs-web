0832d64dda37838ee1e06537f6c0c87b08bb5a4a
Visnu Pitiyanuvath <visnup@gmail.com>
2026-03-21 22:45:28 -0700

    upgrade vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import vercel from "vite-plugin-vercel";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), vercel()],
+  plugins: [react()],
   build: {
     rollupOptions: {
       output: {
