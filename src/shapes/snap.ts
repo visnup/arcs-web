d01e822317ca07bc2010a6cd90a183af78d58290
Visnu Pitiyanuvath <visnup@gmail.com>
2026-04-09 10:59:13 -0700

    fix types

--- src/shapes/snap.ts
+++ src/shapes/snap.ts
@@ -1,5 +1,5 @@
 import { atom } from "tldraw";
-import type { Box, TLShape, VecLike } from "tldraw";
+import type { Box, TLShape, TLShapeId, VecLike } from "tldraw";
 
 export type Snap = {
   x: number; // shape-local center x of snap target
@@ -12,7 +12,7 @@ export type Snap = {
 
 // Current snap being targeted during a drag
 export type SnapTarget = {
-  shapeId: string;
+  shapeId: TLShapeId;
   x: number; // page-space top-left of where shape will snap
   y: number;
   rotation: number;
